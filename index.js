1659882e31eb3f2c5e3703a3e91f4933bbd92214
prechy-cyber <ajaowaleprecious14@gmail.com>
2025-11-17 17:58:08 -0800

    anothercommit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // CORS — allow your frontend deployment URL
 app.use(cors({
-  origin: "https://frontend-six-phi-18.vercel.app", // <-- replace with your Vercel URL
+  origin: "https://frontend-delta-blond-89.vercel.app", // <-- replace with your Vercel URL
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true,
 }));
